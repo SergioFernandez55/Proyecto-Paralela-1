eacdd77f600a896f57b2be464922b2dd5ed356f0
paceruva <40699661+paceruva@users.noreply.github.com>
2018-09-27 13:51:41 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -20,7 +20,7 @@ int main() {
 	cout << endl;
 	while (numPersonas <= 0 || numPersonas > 10000000)
 	{
-		cout << "Numero de personas no válido." << endl << "Ingrese la cantidad de personas (0 a 10.000.000): ";
+		cout << "Numero de personas no valido." << endl << "Ingrese la cantidad de personas (0 a 10.000.000): ";
 		cin >> numPersonas;
 		cout << endl;
 	}
@@ -29,7 +29,7 @@ int main() {
 	cout << endl;
 	while (potenciaVirus <= 0 || potenciaVirus > 99)
 	{
-		cout << "Porcentaje de la potencia de virus no válido" << endl << "Ingrese el porcentaje de la potencia del virus (0 a 99): ";
+		cout << "Porcentaje de la potencia de virus no valido" << endl << "Ingrese el porcentaje de la potencia del virus (0 a 99): ";
 		cin >> potenciaVirus;
 		cout << endl;
 	}
@@ -38,7 +38,7 @@ int main() {
 	cout << endl;
 	while (probRecuperacion <= 0 || probRecuperacion > 99)
 	{
-		cout << "Porcentaje de la de la probabilidad de recuperacion no válido" << endl << "Ingrese el porcentaje de la probabilidad de recuperacion (0 a 99): ";
+		cout << "Porcentaje de la de la probabilidad de recuperacion no valido" << endl << "Ingrese el porcentaje de la probabilidad de recuperacion (0 a 99): ";
 		cin >> probRecuperacion;
 		cout << endl;
 	}
@@ -47,7 +47,7 @@ int main() {
 	cout << endl;
 	while (probMuerte <= 0 || probMuerte > 99)
 	{
-		cout << "Porcentaje de la de la probabilidad de muerte no válido" << endl << "Ingrese el porcentaje de la probabilidad de muerte (0 a 99): ";
+		cout << "Porcentaje de la de la probabilidad de muerte no valido" << endl << "Ingrese el porcentaje de la probabilidad de muerte (0 a 99): ";
 		cin >> probMuerte;
 		cout << endl;
 	}
@@ -56,25 +56,25 @@ int main() {
 	cout << endl;
 	while (porcentajeIntectados <= 0 || porcentajeIntectados > 99)
 	{
-		cout << "Porcentaje de personas infectadas inicialmente no válido" << endl << "Ingrese el porcentaje de personas infectadas inicialmente (0 a 99): ";
+		cout << "Porcentaje de personas infectadas inicialmente no valido" << endl << "Ingrese el porcentaje de personas infectadas inicialmente (0 a 99): ";
 		cin >> porcentajeIntectados;
 		cout << endl;
 	}
-	cout << "Ingrese el tamaño del mundo (100, 500 o 1000): ";
+	cout << "Ingrese el tamaÃ±o del mundo (100, 500 o 1000): ";
 	cin >> tamano;
 	cout << endl;
 	while (tamano != 100 && tamano != 500 && tamano != 1000)
 	{
-		cout << "Tamaño del mundo no válido" << endl << "Ingrese el tamaño del mundo (100, 500 o 1000): ";
+		cout << "TamaÃ±o del mundo no valido" << endl << "Ingrese el tamaÃ±o del mundo (100, 500 o 1000): ";
 		cin >> tamano;
 		cout << endl;
 	}
-	cout << "Ingrese la cantidad de días que va a durar la simulacion" << endl;
+	cout << "Ingrese la cantidad de dias que va a durar la simulacion: ";
 	cin >> duracion;
 	cout << endl;
 	while (duracion <= 0)
 	{
-		cout << "Porcentaje de personas infectadas inicialmente no válido" << endl << "Ingrese el porcentaje de personas infectadas inicialmente (0 a 99): ";
+		cout << "Porcentaje de personas infectadas inicialmente no valido" << endl << "Ingrese el porcentaje de personas infectadas inicialmente (0 a 99): ";
 		cin >> duracion;
 		cout << endl;
 	}
@@ -84,4 +84,6 @@ int main() {
 	for (int salida = 0; salida < salidas.size(); ++salida) {
 
 	}
+	cin >> duracion;
+	return duracion;
 }
